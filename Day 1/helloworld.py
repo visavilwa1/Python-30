8a5729d90b8c1f29afd72ad04faaba73afd9386a
Visavilwa Daisy <135525440+visavilwa1@users.noreply.github.com>
2024-02-13 05:18:31 +0300

    day 1 complete

---
+++ Day 1/helloworld.py
@@ -0,0 +1,18 @@
+#question 1
+print("Hello, World!")
+
+#Question 2
+name = input("Enter your name: ")
+print("Hello, "+ name + "!")
+
+#Question 3
+length = float(input('Enter the leghth of the rectangle: '))
+width = float(input('Enter the width of the rectangle: '))
+area = length * width
+print("The area of the rectangle is:", area)
+
+#question 4 
+radius = float(input('Enter the  radius of the circle: '))
+pi = 3.14159
+area_circle = pi * radius ** 2
+print('This area of the circle is:', area_circle)
